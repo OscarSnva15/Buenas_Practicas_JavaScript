61f83ef9de652a233f35b73d7a822de5f14eb35c
OscarSnva15 <oscarsnva15@gmail.com>
2022-02-27 21:16:02 -0600

    ObjetoEvento-js

---
+++ Crear e insertar elementos Java Script.md
@@ -0,0 +1,7 @@
+# El amigo y vecino EL evento
+
+EL 99 porciento de las veces que trabajemos en los eventos ,vamos a trabajar con el objeto evento.
+
+Este consiste en darnos informacion del evento.
+
+
--- objeto_evento.js
+++ objeto_evento.js
@@ -1,6 +1,23 @@
-const form = document.getElementById('form')
-const input = document-getElementById('input')
-
-input.addEventListener('keyup', (e) => {
-    console.log(e.key)
+const input = document.getElementById('input')
+input.addEventListener('keyup', (eventoParametro)=> {
+    console.log(eventoParametro.explicitOriginalTarget.type);//por rutas con puntos vas navegand
+    //
 })
+
+/*addEventListener('click',(eventoParametro)=>{
+    console.log(eventoParametro);
+})*/
+
+// const button = document.getElementById('button');
+
+// button.addEventListener('click', (eventoParametro)=>{
+//     console.log(eventoParametro.target)
+// })
+
+const container = document.getElementById('container')
+
+container.addEventListener('click',(eventoParametro) => {
+    console.log(eventoParametro.target.textContent)
+    eventoParametro.target.classList.add('classItemColor');
+})
+Form.addEventListener
