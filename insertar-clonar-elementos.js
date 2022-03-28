d295fd46b434f58aaa25bfd848f59b82977acb7b
oscarsnva15 <oscarsnva15@gmail.com>
2022-03-27 20:57:24 -0600

    add: insertat - elimina - elementos

--- insertar-clonar-elementos.js
+++ insertar-clonar-elementos.js
@@ -16,4 +16,28 @@ list.children[0].insertAdjacentElement('aftereend',newElement);
 list.children[1].insertAdjacentElementHtml('afterend',<h1>Hola im a new title element</h1>)
 
 //parent text
-list.children[1].insertAdjacentElementHtml('afterend',<h1>Hola im a new title element</h1>)
+list.children[1].insertAdjacentElementHtml('afterend',"Hola im a text element")
+
+//replace nodes
+list.replacechild(newElement,list.children[0])
+//
+list.children[0].before(newElement)
+//
+list.prepend(newElement)
+//
+list.children[1].after(newElement);
+
+
+
+//remplazar elementos
+document.getElementById('child-to-replace').replaceWith(newElement)
+
+
+///CLONAR ELEMENTOS
+
+console.log(list.cloneNode(true));
+list.after(list.cloneNode(true))///true clona tambien los hijos , false no clonaria los nodos hijos
+
+//Eliminar ELEMENTOS
+list.remove() //nos cargamos la lista completamente
+list.removeChild(list.children[1]);
