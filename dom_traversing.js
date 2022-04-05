bd28b3aa8e6607494b195a7d6d938461cf37e6f5
oscarsnva15@gmail.com <oscarsnva15@gmail.com>
2022-04-05 14:08:22 -0500

    add from  whorkshopoffice: DomTraversing

---
+++ dom_traversing.js
@@ -0,0 +1,9 @@
+//primero hay que obtener el elemento, es facil
+const parent = document.getElementById('parent');
+//propiedades para recorrer el Dom
+
+console.log(parent.parentNode);
+/**
+ * Imprime el padre de un elemento
+ *  
+ */
